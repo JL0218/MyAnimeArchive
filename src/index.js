21b2d84fb5fafe0cebb3f1202a5666e624931670
Jacky Liu <nimbuskycloud@gmail.com>
2022-02-27 01:35:45 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Routing from './routing/Routes';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routing />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
